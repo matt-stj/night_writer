13e9ac2c87bafc5fe830214bc0c708541172ca6c
Matt Stjernholm <thew88@gmail.com>
2015-09-09 11:39:01 -0600

    convert works and outputs to file

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,11 +1,8 @@
 class NightWriter
 
-  im_running_for_real = ($PROGRAM_NAME == __FILE__)
+  im_running_a_test = ($PROGRAM_NAME == __FILE__)
 
-  if im_running_for_real
-  end
-
-  if $PROGRAM_NAME == __FILE__
+  if im_running_a_test
     input = File.open(ARGV[0], "r")
     output = File.open(ARGV[1], 'w')
   else
@@ -17,18 +14,64 @@ class NightWriter
   input.each_line do |line|
     @data << (line.downcase.strip.gsub("\n",''))
   end
-
   input.close
-  output.write(@data.flatten[0])
-  output.close
-  length = @data.inject(0) {|c, w| c += w.length }
 
-  puts "Created '#{ARGV[1]}' containing #{length} characters"
+  def self.to_braille(input)
+    key =    {  "a" => [["0."], [".."], [".."]], "b" => [["0."], ["0."], [".."]],
+                "c" => [["00"], [".."], [".."]], "d" => [["00"], ["0."], [".."]],
+                "e" => [["0."], [".0"], [".."]], "f" => [["00"], ["0."], [".."]],
+                "g" => [["00"], ["00"], [".."]], "h" => [["0."], ["0."], [".."]],
+                "i" => [[".0"], ["0."], [".."]], "j" => [[".0"], ["00"], [".."]],
+                "k" => [["0."], [".."], ["0."]], "l" => [["0."], ["0."], ["0."]],
+                "m" => [["00"], [".."], ["0."]], "n" => [["00"], [".0"], ["0."]],
+                "o" => [["0."], [".0"], ["0."]], "p" => [["00"], ["0."], ["0."]],
+                "q" => [["00"], ["00"], ["0."]], "r" => [["0."], ["00"], ["0."]],
+                "s" => [[".0"], ["0."], ["0."]], "t" => [[".0"], ["00"], ["0."]],
+                "u" => [["0."], [".."], ["00"]], "v" => [["0."], ["0."], ["0."]],
+                "w" => [[".0"], ["00"], [".0"]], "x" => [["00"], ["0."], ["00"]],
+                "y" => [["00"], [".0"], ["00"]], "z" => [["0."], [".0"], ["00"]],
+                " " => [[".."], [".."], [".."]], "." => [[".."], ["00"], ["0."]],
+                "’" => [[".."], ["0."], [".."]], "!" => [[".."], ["00"], ["0."]],
+                "?" => [[".."], ["0."], ["00"]], "'" => [[".."], [".."], ["0."]],
+                "-" => [[".."], [".."], ["00"]]
 
+              }
 
-  def self.to_braille(file)
+    string = @data[0]
+    split_string = string.split("")
+
+    new_line_1 = []
+    line_1 = split_string.each do |x|
+      new_line_1.push(key[x][0])
+    end
+    flat_line_1 = new_line_1.flatten
+    final_line_1 = flat_line_1.join("")
+
+    new_line_2 = []
+    line_2 = split_string.each do |x|
+      new_line_2.push(key[x][1])
+      flat_2 = new_line_2.flatten
+    end
+    flat_line_2 = new_line_2.flatten
+    final_line_2 = flat_line_2.join("")
+
+    new_line_3 = []
+    line_3 = split_string.each do |x|
+      new_line_3.push(key[x][2])
+      flat_3 = new_line_3.flatten
+    end
+    flat_line_3 = new_line_3.flatten
+    final_line_3 = flat_line_3.join("")
+
+    @block = [final_line_1, final_line_2, final_line_3].join("\n")
   end
 
-end
+  braille = NightWriter.to_braille(@data)
 
-braille = NightWriter.to_braille(input)
+  output.write(@block)
+  output.close
+  output_length = @block.chars.inject(0) {|c, w| c += w.length }
+
+  puts "Created '#{ARGV[1]}' containing #{output_length} characters"
+
+end
--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -1,9 +1,10 @@
-require 'minitest'
-require_relative '../lib/night_write'
+require 'minitest'                     # => true
+require_relative '../lib/night_write'  # ~> TypeError: no implicit conversion of nil into String
 
 class NightWriterTest < Minitest::Test
 
   def test_we_can_import_a_file
+    skip
     `ruby ../lib/night_write.rb ../input/message.txt ../output/writer.rb`
   end
 
@@ -17,17 +18,29 @@ class NightWriterTest < Minitest::Test
 
   def test_we_can_convert_a_single_character_to_braille_in_one_flat_line
     skip
-    assert_equal "sakljd",
   end
 
   def test_we_can_output_the_conversion_into_3_lines
+    skip
   end
 
   def test_we_can_convert_a_string_of_test_to_functional_braille
+    skip
     #### input output for hello world ####
     # "hello world"
     # "0.0.0.0.0....00.0.0.00" == "0.0.0.0.0....00.0.0.00"
     # "0..00.0..0..00.0000.0." == "0..00.0..0..00.0000.0."
     # "....0.0.0....00.0.0..." == "....0.0.0....00.0.0..."
   end
+
 end
+
+# ~> TypeError
+# ~> no implicit conversion of nil into String
+# ~>
+# ~> /Users/Matt/Turing/1-Modual/Projects/night_writer/lib/night_write.rb:9:in `initialize'
+# ~> /Users/Matt/Turing/1-Modual/Projects/night_writer/lib/night_write.rb:9:in `open'
+# ~> /Users/Matt/Turing/1-Modual/Projects/night_writer/lib/night_write.rb:9:in `<class:NightWriter>'
+# ~> /Users/Matt/Turing/1-Modual/Projects/night_writer/lib/night_write.rb:1:in `<top (required)>'
+# ~> /Users/Matt/Turing/1-Modual/Projects/night_writer/test/night_write_test.rb:2:in `require_relative'
+# ~> /Users/Matt/Turing/1-Modual/Projects/night_writer/test/night_write_test.rb:2:in `<main>'
